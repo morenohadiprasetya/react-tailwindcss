96859f57ce70dc2165d2cb15951977bb6dae6fa6
morenohadiprasetya <ambamuani@gmail.com>
2025-09-12 00:41:49 -0700

    membuat registerpage

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -18,7 +18,10 @@ function Register() {
     };
 
     return (
-        <div className="flex item-center justify-center min-h-screen bg-gray-100">
+       
+       
+       
+       <div className="flex item-center justify-center min-h-screen bg-gray-100">
         <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
             <h1 className="text-2xl font-bold text-center mb-6">Login</h1>
             <form onSubmit={handleSubmit}>
